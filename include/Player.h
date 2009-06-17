8a2de604caac0243be6e98b587936b7c51dfc348
Anders Andersson <trezker@gmail.com>
2009-06-17 23:17:37 +0200

    Player alternative control

--- include/Player.h
+++ include/Player.h
@@ -8,6 +8,59 @@
 #include <allegro5/allegro5.h>
 #include <zoidcom/zoidcom.h>
 
+// the input class
+struct Input
+{
+  Input() { 
+    up = down = left = right = false; 
+  }
+
+  // write input to bitstream
+  void pack(ZCom_BitStream &_str) {
+    _str.addBool(up);
+    _str.addBool(down);
+    _str.addBool(left);
+    _str.addBool(right);
+  }
+
+  // read input from bitstream
+  void unpack(ZCom_BitStream &_str) {
+    up = _str.getBool();
+    down = _str.getBool();
+    left = _str.getBool();
+    right = _str.getBool();
+  }
+
+  bool operator==(const Input& _in) {
+    return (up == _in.up && down == _in.down && left == _in.left && right == _in.right);
+  }
+
+  Input& operator=(const Input& _rhs) {
+    up = _rhs.up; down = _rhs.down; left = _rhs.left; right = _rhs.right;
+    return *this;
+  }
+
+  bool up;
+  bool down;
+  bool left;
+  bool right;
+};
+
+class Net_movement: public ZCom_MoveUpdateListener<zFloat>
+{
+public:
+	Net_movement(ZCom_Node* net_node);
+	void inputUpdated(ZCom_BitStream& _inputstream, bool _inputchanged, zU32 _client_time, zU32 _estimated_time_sent);
+	void inputSent(ZCom_BitStream& _inputstream);
+	void correctionReceived(zFloat *_pos, zFloat* _vel, zFloat *_acc, bool _teleport, zU32 _estimated_time_sent);
+	void updateReceived(ZCom_BitStream& _inputstream, zFloat *_pos, zFloat* _vel, zFloat *_acc, zU32 _estimated_time_sent) {}
+
+	Vector3 position;
+	ZCom_Replicate_Movement<zFloat, 3> *moverep;
+	Input last_input_received;
+	Input last_input_sent;
+};
+
 class Player: public Scenenode
 {
 public:
@@ -44,13 +97,15 @@ private:
 	bool move_right;
 	bool move_up;
 	bool move_down;
-	
+
 	float health;
 	int score;
 	bool dropping_bomb;
-	
+
 	ZCom_Node* net_node;
 	bool deleteme;
+
+	Net_movement *net_movement;
 };
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -80,31 +80,46 @@ void Player::Update(double dt, Vector3 camera_right, Vector3 camera_front, Heigh
 
 	if(net_node && net_node->getRole() ==  eZCom_RoleProxy)
 	{
-		transform->Set_rotation(Vector3(0, angle, 0));
+		transform->Set_rotation(Vector3(0, angle*180/M_PI, 0));
+
 		Set_position(position);
 	}
 	else
 	{
-		Vector3 direction(move_left-move_right, 0/*move_up-move_down*/, move_forward-move_backward);
-		direction.Normalize();
-
-		Vector3 right = camera_right;
-		right.y = 0;
-		right.Normalize();
-		Vector3 front = camera_front;
-		front.y = 0;
-		front.Normalize();
-
 		float speed_factor = 10*dt;
+		float rotspeed_factor = M_PI*dt;
 		Vector3 speed;
-		speed += right*direction.x;
-	//	speed += Vector3(0, 1, 0)*direction.y;
-		speed += front*direction.z;
+
+		int control = 1;
+		if(control == 1)
+		{
+			angle += (move_left-move_right) * rotspeed_factor;
+			speed.Set(	sin(angle)*(move_forward-move_backward),
+						0/*move_up-move_down*/,
+						cos(angle)*(move_forward-move_backward));
+			transform->Set_rotation(Vector3(0, angle*180/M_PI, 0));
+		}
+		if(control == 0)
+		{
+			Vector3 direction(move_left-move_right, 0/*move_up-move_down*/, move_forward-move_backward);
+			direction.Normalize();
+
+			Vector3 right = camera_right;
+			right.y = 0;
+			right.Normalize();
+			Vector3 front = camera_front;
+			front.y = 0;
+			front.Normalize();
+
+			speed += right*direction.x;
+		//	speed += Vector3(0, 1, 0)*direction.y;
+			speed += front*direction.z;
+			angle = atan2(speed.x, speed.z);
+			transform->Set_rotation(Vector3(0, angle*180/M_PI, 0));
+		}
 
 		if(speed != Vector3::ZERO)
 		{
-			angle = atan2(speed.x, speed.z)*180/M_PI;
-			transform->Set_rotation(Vector3(0, angle, 0));
 			Vector3 map_normal = heightmap->Get_normal(Get_position().x, Get_position().z);
 			map_normal.y = 0;
 			if(Get_position().y<=heightmap->Get_height(Get_position().x, Get_position().z))
@@ -231,16 +246,20 @@ void Player::Drop_bomb()
 
 ZCom_Node* Player::Register_net_node(ZCom_Control *control, ZCom_ClassID class_id)
 {
+
 	net_node = new ZCom_Node;
 
-	ZCom_BitStream *adata = new ZCom_BitStream();
 	Vector3 pos = Get_position();
+	ZCom_BitStream *adata = new ZCom_BitStream();
 	adata->addFloat(pos.x, POSITION_MANTISSA);
 	adata->addFloat(pos.y, POSITION_MANTISSA);
 	adata->addFloat(pos.z, POSITION_MANTISSA);
 	net_node->setAnnounceData(adata);
 
 	net_node->beginReplicationSetup(5);
+
+
+
 	net_node->addReplicationFloat(&health, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_ALL);
 	net_node->addReplicationInt(&score, 10, false, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_ALL);
 	
@@ -249,6 +268,7 @@ ZCom_Node* Player::Register_net_node(ZCom_Control *control, ZCom_ClassID class_i
 	net_node->addInterpolationFloat(&position.x, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_PROXY|ZCOM_REPRULE_OWNER_2_AUTH, 1, &interpos.x);
 	net_node->addInterpolationFloat(&position.y, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_PROXY|ZCOM_REPRULE_OWNER_2_AUTH, 1, &interpos.y);
 	net_node->addInterpolationFloat(&position.z, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_PROXY|ZCOM_REPRULE_OWNER_2_AUTH, 1, &interpos.z);
+
 /*
 	net_node->addReplicationFloat(&position.x, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_PROXY|ZCOM_REPRULE_OWNER_2_AUTH);
 	net_node->addReplicationFloat(&position.y, 10, ZCOM_REPFLAG_MOSTRECENT, ZCOM_REPRULE_AUTH_2_PROXY|ZCOM_REPRULE_OWNER_2_AUTH);
@@ -303,4 +323,46 @@ void Player::Process_net_events()
 bool Player::Deleteme()
 {
 	return deleteme;
+}
+
+
+
+/*
+ * Net_movement functions
+*/
+
+Net_movement::Net_movement(ZCom_Node* net_node)
+{
+	// create the movement replicator
+	moverep = new ZCom_Replicate_Movement<zFloat, 3>(32,
+	ZCOM_REPFLAG_MOSTRECENT|ZCOM_REPFLAG_SETUPPERSISTS,
+	ZCOM_REPRULE_OWNER_2_AUTH|ZCOM_REPRULE_AUTH_2_PROXY);
+	// set a low error threshold
+	((ZCom_RSetupMovement<zFloat>*)moverep->getSetup())->setConstantErrorThreshold(0);
+	// set interpolation time to 50 milliseconds
+	((ZCom_RSetupMovement<zS32>*)moverep->getSetup())->setInterpolationTime(50);
+	// apply timescale
+	moverep->setTimeScale((float)50/1000.0f);
+
+	// add the replicator
+	net_node->addReplicator(moverep, true);
+
+	// set update listener (the methods NObject::inputUpdated, NObject::inputSent, NObject::correctionReceived,
+	// and NObject::updateReceived implement the update listener interface and are called by moverep)
+	moverep->setUpdateListener(this);
+
+	// set initial position
+	moverep->updateState((zFloat*) position.vector, NULL, NULL, false);
+}
+
+void Net_movement::inputUpdated(ZCom_BitStream& _inputstream, bool _inputchanged, zU32 _client_time, zU32 _estimated_time_sent)
+{
+}
+
+void Net_movement::inputSent(ZCom_BitStream& _inputstream)
+{
+}
+
+void Net_movement::correctionReceived(zFloat *_pos, zFloat* _vel, zFloat *_acc, bool _teleport, zU32 _estimated_time_sent)
+{
 }
